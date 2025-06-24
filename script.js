fe3361d5d112ec62fd77be467799d383f38d3874
pipo.corp <yusato8884@icloud.com>
2025-06-24 10:24:09 +0900

    バグ修正中

--- script.js
+++
@@ -1,9 +0,0 @@
-function viewstory() {
-  const viewer = document.getElementById("viewer");
-  const viewnum = document.getElementById("viewnum");
-  viewer.src = "/story/" + viewnum.value + ".html";
-}
-
-document.getElementById("viewnum").addEventListener("change", viewstory);
-
-document.addEventListener("DCMContentLoaded", viewstory)
